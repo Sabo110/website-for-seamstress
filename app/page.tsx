d47ae3dcbc2ecc0c3dbb1b2cd3019edced2a5d28
sabo110 <samirphanuel@gmail.com>
2025-04-13 01:05:24 +0100

    ajout des meta data

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import Hero from "@/components/Hero";
 import Realizations from "@/components/Realizations";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: "Samira Couture - Créations sur mesure et élégance",
+  description: "Bienvenue chez Samira Couture. Découvrez nos créations uniques et sur mesure, alliant élégance, qualité et créativité. Transformez vos idées en pièces d'exception.",
+};
+
 
 export default function Home() {
   return (
